55bca133a9b6aebd5bbc288476d3fceae7dc026f
mitra999 <57156351+mitra999@users.noreply.github.com>
2021-09-25 02:11:37 -0400

    Update ContourMap.py

--- ContourMap.py
+++ ContourMap.py
@@ -1,3 +1,4 @@
+#importing necessary libraries
 import pandas as pd
 import numpy as np
 from scipy.interpolate import griddata
@@ -5,12 +6,15 @@ import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 import matplotlib.cm as cm
 
+#Read input file from dircetory
 df_xyz = pd.read_csv('C:/.....csv')
 
+#Define variables
 x = df_xyz.iloc[:,0].values
 y = df_xyz.iloc[:,1].values
 z = df_xyz.iloc[:,2].values
 
+#Define contour plot function
 def plot_contour(x,y,z,resolution = 100,contour_method='linear'):
     resolution = str(resolution)+'j'
     X,Y = np.mgrid[min(x):max(x):complex(resolution),   min(y):max(y):complex(resolution)]
@@ -21,31 +25,20 @@ def plot_contour(x,y,z,resolution = 100,contour_method='linear'):
 X,Y,Z = plot_contour(x,y,z,resolution = 100,contour_method='linear')
 
 #***--------Ploting Contour--------***--------------------
-
-'''
-with plt.style.context("classic"):
-    
-    fig, ax = plt.subplots(figsize=(13,8))
-       #ax.scatter(x,y, color="black", linewidth=1, edgecolor="none", s=50) # no scater plot behind 
-    ax.contourf(X,Y,Z)
-plt.show()
-'''
-
-levels = [0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6] #set the contour levels
-#plt.scatter(x,y, color="black", linewidth=1, edgecolor="none", s=50) # no scater plot behind 
-
-
-contour = plt.contour(X, Y, Z, levels, colors='k') # plot the contour
-
-plt.clabel(contour, colors = 'k', fmt = '%2.1f', fontsize=10)# add number and editing options for contour levels 
-
-c= ('#363093','#069ccf','#37b89d','#a5be6a','#fdbe3d','#f8fa0d','#f80000')# set individual color for contour levels
-   
-contour_filled = plt.contourf(X, Y, Z, levels, colors=c) # fill color between contour levels
+#set the contour levels
+levels = [0,0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+#set scater plot behind
+plt.scatter(x,y, color="black", linewidth=1, edgecolor="none", s=50)  
+#plot the contour
+contour = plt.contour(X, Y, Z, levels, colors='k') 
+#add number and editing options for contour levels 
+plt.clabel(contour, colors = 'k', fmt = '%2.1f', fontsize=10)
+#set individual color for contour levels
+c= ('#363093','#069ccf','#37b89d','#a5be6a','#fdbe3d','#f8fa0d','#f80000')
+#fill color between contour levels  
+contour_filled = plt.contourf(X, Y, Z, levels, colors=c) 
 plt.colorbar(contour_filled)
-
-plt.scatter(x,y, color="black", linewidth=1, edgecolor="none", s=10) # no scater plot behind 
-
+plt.scatter(x,y, color="black", linewidth=1, edgecolor="none", s=10)
 plt.title('Zeta v. Flow and Speed with Channel')# set title for the plot 
 plt.xlabel('$\omega$ (RPM)') # add lable for x
 plt.ylabel('Q (L/s)') # add lable for y
